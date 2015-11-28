c7291d1710cdf7f6261f0c82130c1e0207471576
future-challenger <sunshine_anycall@hotmail.com>
2015-11-28 18:52:23 +0800

    fragments

--- app/src/main/java/test/cn/myapplication/MainActivity.java
+++ app/src/main/java/test/cn/myapplication/MainActivity.java
@@ -1,13 +1,31 @@
 package test.cn.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
+import android.util.SparseIntArray;
+
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getName();
+
+    private SparseArray<Fragment> mFragmentArray = new SparseArray<Fragment>(4);
+    private SparseIntArray mTabIndexArray = new SparseIntArray(5);
+    private int mCurrentFragmentIndex;  // currently selected fragment
+    private int mClickedFragmentIndex;  // just clicked fragment
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    private void setUpFragments(Bundle savedInstanceState) {
+if(mFragmentArray.size() != 4){
+    mFragmentArray.put(R.id.);
+}
     }
 }
